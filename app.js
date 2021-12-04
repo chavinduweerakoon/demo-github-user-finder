67a77e66466d80d2c3cdb06073b6025ced8d4bd1
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-12-04 14:41:26 +0530

    mapping

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const github = new Github();
+const ui = new UI();
 
 const searchString = document.getElementById("searchUser");
 
@@ -6,11 +7,12 @@ searchString.addEventListener("keyup", (e) => {
   const userText = e.target.value;
   if (userText !== "") {
     github.getUser(userText).then((data) => {
-      console.log(data);
+      //   console.log(data);
       if (data.profile.message === "Not Found") {
         //alert
       } else {
         //profile
+        ui.showProfile(data.profile);
       }
     });
   } else {
